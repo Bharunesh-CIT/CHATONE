62a4a527bff651e49ad5b671c638e9d81b6714a4
Bharunesh-CIT <knightbharu@gmail.com>
2025-06-07 21:17:20 +0530

    first commit

---
+++ app/app.js
@@ -0,0 +1,20 @@
+const socket = io();
+const chatForm = document.getElementById('chat-form');
+const msgInput = document.getElementById('msg-input');
+const chatBox = document.getElementById('chat-box');
+
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const msg = msgInput.value;
+  if (msg.trim()) {
+    socket.emit('chat message', msg);
+    msgInput.value = '';
+  }
+});
+
+socket.on('chat message', (msg) => {
+  const div = document.createElement('div');
+  div.textContent = msg;
+  chatBox.appendChild(div);
+  chatBox.scrollTop = chatBox.scrollHeight;
+});
